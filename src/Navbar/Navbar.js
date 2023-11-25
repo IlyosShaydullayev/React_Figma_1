98b5d43fe843ffde4c593c7bb4df97906979dbba
ozod07 <xolmurodovozodbek96@gmail.com>
2023-11-26 00:06:30 +0500

    navbar created

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,14 +5,44 @@ function Navbar() {
   return (
     <div>
         <header>
-            <nav>
-                <div className='nav-navbar'>
-                    <img src="./images/city.png" alt="" />
-                    <p className='p-title'>Бесплатная доставка заказов до конца декабря!</p>
-                </div>
-                <div className='nav-body'></div>
-                <div className='nav-footer'></div>
-            </nav>
+          <div className="top">
+            <img src="images/city.png" alt="" className="rasm" />
+            <h2>Бесплатная доставка заказов до конца декабря!</h2>
+          </div>
+          <div className="navbar">
+            <img src="images/ezy.svg" alt="" />
+            <div className="items">
+              <p >О нас</p>
+              <p >Помощь</p>
+              <p >Продавцу</p>
+              <p >Покупателю</p>
+              <p >Контакты</p>
+            </div>
+            <div className="search-part">
+              <input type="text" placeholder="Введите номер детали, название или VIN" />
+                <i className="fa-solid fa-clock-rotate-left"></i>
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div className="user-part">
+              <div>
+                <img src="images/Group 159.svg" alt="" className="icon" />
+              </div>
+              <div>
+                <img src="images/Group (2).svg" alt="" className="icon" />
+              </div>
+            </div>
+          </div>
+          <div className="categories">
+            <p>Каталог</p>
+            <p>ТО  / фильтра</p>
+            <p>Тормозная система</p>
+            <p>Двигатель  / выхлоп</p>
+            <p>Коробка передач</p>
+            <p>Охлождение  / отопление</p>
+            <p>Электрика  /освещение</p>
+            <p>Кузов  /элементы</p>
+            
+          </div>
         </header>
     </div>
   )
