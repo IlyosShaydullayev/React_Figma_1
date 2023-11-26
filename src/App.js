30063726c74e4d009775ac68398155ad8b9c5866
Ilyos <ilyosshaydullaye212@gmail.com>
2023-11-26 21:19:36 +0500

    last-endings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Page1 from './pages/page1/Page1'
+// import Page1 from './pages/page1/Page1'
+import Page4 from './pages/page4/Page4';
 
 function App() {
   return (
     <Router>
-      <Page1/>
+      <Page4/>
       <Routes>
         <Route></Route>
       </Routes>
